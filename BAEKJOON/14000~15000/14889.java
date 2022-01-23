2fdfd28a83c88f785c8afdbbf627984b01eb01c5
enyung <45346786+eyl056@users.noreply.github.com>
2022-01-23 20:38:01 +0900

    스타트와 링크

--- BAEKJOON/14000~15000/14889.java
+++ BAEKJOON/14000~15000/14889.java
@@ -1,41 +1,14 @@
-import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
+
     static int N;
     static int[][] arr;
     static boolean[] visited;
     static int min = Integer.MAX_VALUE;
-
-    static void permutation(int[][] arr, boolean[] visited, int idx, int depth, int n) {
-        if (depth == n / 2) {
-            int start = 0;
-            int link = 0;
-            for (int i = 0; i < n - 1; i++) {
-                for (int j = i + 1; j < n; j++) {
-                    if (visited[i] && visited[j]) {
-                        start += arr[i][j];
-                        start += arr[j][i];
-                    } else if (!visited[i] && !visited[j]) {
-                        link += arr[i][j];
-                        link += arr[j][i];
-                    }
-                }
-            }
-            int num = Math.abs(start - link);
-            min = Math.min(min, num);
-            return;
-        }
-
-        for (int i = idx; i < n; i++) {
-            if (visited[i]) continue;
-            visited[i] = true;
-            permutation(arr, visited, idx + 1, depth + 1, n);
-            visited[i] = false;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -45,17 +18,56 @@ public class Main {
         arr = new int[N][N];
         visited = new boolean[N];
 
+
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
             for (int j = 0; j < N; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        permutation(arr, visited, 0, 0, N);
+        permutation(0, 0);
+        System.out.println(min);
 
-        System.out.print(min);
+    }
 
-        br.close();
+    static void permutation(int idx, int depth) {
+        if(depth == N / 2) {
+            difference();
+            return;
+        }
+
+        for(int i = idx; i < N; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                permutation(i + 1, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    static void difference() {
+        int start = 0;
+        int link = 0;
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (visited[i] == true && visited[j] == true) {
+                    start += arr[i][j];
+                    start += arr[j][i];
+                }
+                else if (visited[i] == false && visited[j] == false) {
+                    link += arr[i][j];
+                    link += arr[j][i];
+                }
+            }
+        }
+        int num = Math.abs(start - link);
+        if (num == 0) {
+            System.out.println(num);
+            System.exit(0);
+        }
+        min = Math.min(num, min);
     }
 }
