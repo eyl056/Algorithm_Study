a5d8f57945a2cdf8370998382e754d61c5f86b61
enyung <45346786+eyl056@users.noreply.github.com>
2022-01-27 02:32:15 +0900

    이름 궁합

--- BAEKJOON/15000~16000/15312.java
+++ BAEKJOON/15000~16000/15312.java
@@ -5,18 +5,19 @@ import java.io.InputStreamReader;
 public class Main {
     static int[] count = {3, 2, 1, 2, 3, 3, 2, 3, 3, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1};
     static String name1, name2;
+
     static String collaboration(String name1, String name2) {
         String collabo = "";
+        String collaboStr = "";
         for (int i = 0; i < name1.length(); i++) {
             collabo += name1.charAt(i);
             collabo += name2.charAt(i);
         }
-
-        String collaboStr = "";
-        for (int i = 0; i < 26; i++) {
-            for (int j = 0; j < collabo.length(); j++) {
-                if (count[i] == (collabo.charAt(j) - 'A')) {
-                    collaboStr += count[i];
+        
+        for (int i = 0; i < collabo.length(); i++) {
+            for (int j = 0; j < 26; j++) {
+                if ((collabo.charAt(i) - 'A') == j) {
+                    collaboStr += count[j];
                 }
             }
         }
@@ -25,12 +26,12 @@ public class Main {
             String collaboSstr = "";
             for (int i = 0; i < collaboStr.length() - 1; i++) {
                 int sum = Integer.parseInt(String.valueOf(collaboStr.charAt(i))) + Integer.parseInt(String.valueOf(collaboStr.charAt(i + 1)));
-                collaboSstr = String.valueOf(sum % 10);
+                collaboSstr += String.valueOf(sum % 10);
             }
-            collabo = collaboSstr;
+            collaboStr = collaboSstr;
         }
 
-        return collabo;
+        return collaboStr;
     }
 
     public static void main(String[] args) throws IOException {
