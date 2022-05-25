69be914af0ca97ae004c076a27bb8600c5e08748
enyung <45346786+eyl056@users.noreply.github.com>
2022-05-26 03:40:43 +0900

    Update 신고 결과 받기.java

--- Programmers/Lv.1/신고 결과 받기.java
+++ Programmers/Lv.1/신고 결과 받기.java
@@ -1,50 +1,30 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         ArrayList<String> rReport = new ArrayList<>();
         
-        // 여러번 신고하는 중복 제거
-        for (String reportTo : report) {
-            if(!rReport.contains(reportTo))
-                rReport.add(reportTo);
-        }
-        report = new String[rReport.size()];
-        int size = 0;
-        for (String temp : rReport) {
-            report[size++] = temp;
-        }
-        
-        // 신고된 횟수 취합하기
-        int[] count = new int[id_list.length];
-        
-        for (int i = 0; i < report.length; i++) {
-            String[] reportTo = report[i].split(" ");
-            
-            for (int j = 0; j < id_list.length; j++) {
-                if (id_list[j].equals(reportTo[1])) count[j]++;
-            }
-        }
-        
-        // 정지된 유저: ban
-        ArrayList<String> ban = new ArrayList<>();
-        
-        for (int i = 0; i < id_list.length; i++) {
-            if (count[i] >= k) ban.add(id_list[i]);
-        }
-        System.out.print("신고된 유저: ");
-        System.out.println(ban);
-        
-        // 다시 report 돌면서 확인
         int[] answer = new int[id_list.length];
         
-        for (int i = 0; i < report.length; i++) {
-            String[] reportTo = report[i].split(" ");
-            
-            if (ban.contains(reportTo[1])) {
-                for (int j = 0; j < id_list.length; j++) {
-                    if (id_list[j].equals(reportTo[0])) answer[j]++;
+        // <신고 당한 유저, 신고한 유저 집합>
+        HashMap<String, HashSet<String>> reportTo = new HashMap<>();
+        HashMap<String, Integer> id = new HashMap<>();
+        
+        for (int i = 0; i < id_list.length; i++) {
+            reportTo.put(id_list[i], new HashSet<>());
+            id.put(id_list[i], i);
+        }
+        
+        for (String tmp : report) {
+            String[] str = tmp.split(" ");
+            reportTo.get(str[1]).add(str[0]);
+        }
+        
+        for (int i = 0; i < id_list.length; i++) {
+            HashSet<String> from = reportTo.get(id_list[i]);
+            if (from.size() >= k) {
+                for (String name : from) {
+                    answer[id.get(name)]++;
                 }
             }
         }
